c738a168d18fdb7f4216d61fcf266bc6408d3e06
pedroblome <joaopedroblome78@gmail.com>
2022-03-23 00:25:49 -0300

    adding sonarCloud validations

--- src/main/java/com/pedroblome/user/controller/UserOrderController.java
+++ src/main/java/com/pedroblome/user/controller/UserOrderController.java
@@ -10,7 +10,6 @@ import com.pedroblome.user.service.UserOrderService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins ={"http://localhost:8081/"})
 @RestController
 @RequestMapping("/userOrder")
 public class UserOrderController {
